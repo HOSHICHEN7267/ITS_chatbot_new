30eae1f57799837827a1d41d7975fbb59e8ee361
jcyunmrrr <jsmn303@gmail.com>
2024-06-17 17:38:44 +0800

    Modify error message

--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -7,8 +7,12 @@ import 'openai_send_unit.dart' as openai_send_unit;
 import 'network_unit.dart' as network_unit;
 
 final logger = Logger();
-final connectErrorMessage = jsonEncode(
-    {'result': false, 'data': '抱歉，網路好像有點狀況QQ\n可以麻煩你檢查一下網路嗎？好了的話直接回來和我聊天吧'});
+final connectErrorMessage = jsonEncode({
+  'result': false,
+  'data': '''抱歉，網路好像有點狀況QQ
+
+可以麻煩你檢查一下網路嗎？好了的話直接回來和我聊天吧'''
+});
 
 class ApiManager {
   TdxUnit tdxUnit = TdxUnit();
@@ -27,16 +31,19 @@ class ApiManager {
     String AItoMAP = await openai_receive_unit.getResult(inputString);
     // logger.i('AItoMAP: $AItoMAP');
     if (!jsonDecode(AItoMAP)['result']) {
-      if (jsonDecode(AItoMAP)['message'] == 'origin or destination is not correct'){
+      if (jsonDecode(AItoMAP)['message'] ==
+          'origin or destination is not correct') {
         return jsonEncode({
           'result': false,
-          'data': '抱歉，我沒有聽懂你的起點及目的地，分別在哪裡QQ\n可以再告訴我一次：你的起點、目的地，以及希望省錢還是省時間嗎？'
+          'data': '''抱歉，我沒有聽懂你的起點及目的地，分別在哪裡QQ
+
+可以再告訴我一次：你的起點、目的地，以及希望省錢還是省時間嗎？'''
         });
-      }
-      else {
+      } else {
         return jsonEncode({
           'result': false,
-          'data': '抱歉，連接 open ai 時出現問題，錯誤訊息：${jsonDecode(AItoMAP)['message']}'
+          'data':
+              '''抱歉，連接 open ai 時出現問題。錯誤訊息：${jsonDecode(AItoMAP)['message']}'''
         });
       }
     }
@@ -46,7 +53,9 @@ class ApiManager {
     if (!jsonDecode(MAPtoTDX)['result']) {
       return jsonEncode({
         'result': false,
-        'data': '抱歉，你的起點及目的地，似乎有無法在地圖上搜尋到的地方QQ\n可以再告訴我一次：你的起點、目的地，以及希望省錢還是省時間嗎？'
+        'data': '''抱歉，你的起點及目的地，似乎有無法在地圖上搜尋到的地方QQ
+
+可以再告訴我一次：你的起點、目的地，以及希望省錢還是省時間嗎？'''
       });
     }
 
@@ -57,14 +66,20 @@ class ApiManager {
       if (jsonDecode(TDXtoAI)['message'] == 'Route not found') {
         return jsonEncode({
           'result': false,
-          'data':
-              '抱歉，我只會規劃台灣境內的交通路線，如果你要出國的話，我就無法給你幫助了QQ\n如果你還想去其他台灣地點的話，可以再告訴我一次：你的起點、目的地，以及希望省錢還是省時間嗎？'
+          'data': '''抱歉，看起來這超出了我的能力範圍，無法給你幫助QQ
+
+你可以試試這些方法，幫助我更好地找到正確的路線：
+
+1. 對地點更詳細的描述：比起**市政府**，**臺南市政府**會是更好的選擇！
+2. 避免輸入國外地點：我只能協助規劃臺灣境內的路線
+3. 起終點附近的大眾運輸：有些地方大眾運輸到不了，我就沒辦法規劃了
+
+確認過上面幾點後，可以再告訴我一次：你的起點、目的地，以及希望省錢還是省時間嗎？'''
         });
-      }
-      else {
+      } else {
         return jsonEncode({
           'result': false,
-          'data': '抱歉，連接 TDX 時出現問題，錯誤訊息：${jsonDecode(TDXtoAI)['message']}'
+          'data': '''抱歉，連接 TDX 時出現問題。錯誤訊息：${jsonDecode(TDXtoAI)['message']}'''
         });
       }
     }
@@ -76,7 +91,9 @@ class ApiManager {
     if (!jsonDecode(AItoUSER)['result']) {
       return jsonEncode({
         'result': false,
-        'data': '抱歉，小幫手在產生交通路線時，出了一點問題QQ\n可以再告訴我一次：你的起點、目的地，以及希望省錢還是省時間嗎？'
+        'data': '''抱歉，小幫手在產生交通路線時，出了一點問題QQ
+
+可以再告訴我一次：你的起點、目的地，以及希望省錢還是省時間嗎？'''
       });
     }
 
