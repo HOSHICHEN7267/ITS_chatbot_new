c0e10979c349c73d403688033d431a60b37fb860
AC <109703034@g.nccu.edu.tw>
2024-06-17 15:07:36 +0800

    更詳細的錯誤訊息回應

--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -4,7 +4,7 @@ import 'openai_receive_unit.dart' as openai_receive_unit;
 import 'map_unit.dart' as map_unit;
 import 'tdx_unit.dart.';
 import 'openai_send_unit.dart' as openai_send_unit;
-import 'network_unit.dart' as network_unit;
+// import 'network_unit.dart' as network_unit;
 
 final logger = Logger();
 final connectErrorMessage = jsonEncode(
@@ -14,9 +14,9 @@ class ApiManager {
   TdxUnit tdxUnit = TdxUnit();
 
   Future<String> getResult(String inputString) async {
-    if (!(await network_unit.isConnect())) {
-      return connectErrorMessage;
-    }
+    // if (!(await network_unit.isConnect())) {
+    //   return connectErrorMessage;
+    // }
 
     // String error_message = "抱歉，我有點看不懂QQ\n要再麻煩你告訴我一次你的起點、目的地，以及希望省錢還是省時間喔！";
 
@@ -25,16 +25,24 @@ class ApiManager {
     }
 
     String AItoMAP = await openai_receive_unit.getResult(inputString);
-    logger.i('AItoMAP: $AItoMAP');
+    // logger.i('AItoMAP: $AItoMAP');
     if (!jsonDecode(AItoMAP)['result']) {
-      return jsonEncode({
-        'result': false,
-        'data': '抱歉，我沒有聽懂你的起點及目的地，分別在哪裡QQ\n可以再告訴我一次：你的起點、目的地，以及希望省錢還是省時間嗎？'
-      });
+      if (jsonDecode(AItoMAP)['message'] == 'origin or destination is not correct'){
+        return jsonEncode({
+          'result': false,
+          'data': '抱歉，我沒有聽懂你的起點及目的地，分別在哪裡QQ\n可以再告訴我一次：你的起點、目的地，以及希望省錢還是省時間嗎？'
+        });
+      }
+      else {
+        return jsonEncode({
+          'result': false,
+          'data': '抱歉，連接 open ai 時出現問題，錯誤訊息：${jsonDecode(AItoMAP)['message']}'
+        });
+      }
     }
 
     String MAPtoTDX = await map_unit.getResult(AItoMAP);
-    logger.i('MAPtoTDX: $MAPtoTDX');
+    // logger.i('MAPtoTDX: $MAPtoTDX');
     if (!jsonDecode(MAPtoTDX)['result']) {
       return jsonEncode({
         'result': false,
@@ -46,11 +54,19 @@ class ApiManager {
     // logger.i('TDXtoAI: $TDXtoAI');
     // print('TDXtoAI: Received');
     if (!jsonDecode(TDXtoAI)['result']) {
-      return jsonEncode({
-        'result': false,
-        'data':
-            '抱歉，我只會規劃台灣境內的交通路線，如果你要出國的話，我就無法給你幫助了QQ\n如果你還想去其他台灣地點的話，可以再告訴我一次：你的起點、目的地，以及希望省錢還是省時間嗎？'
-      });
+      if (jsonDecode(TDXtoAI)['message'] == 'Route not found') {
+        return jsonEncode({
+          'result': false,
+          'data':
+              '抱歉，我只會規劃台灣境內的交通路線，如果你要出國的話，我就無法給你幫助了QQ\n如果你還想去其他台灣地點的話，可以再告訴我一次：你的起點、目的地，以及希望省錢還是省時間嗎？'
+        });
+      }
+      else {
+        return jsonEncode({
+          'result': false,
+          'data': '抱歉，連接 TDX 時出現問題，錯誤訊息：${jsonDecode(TDXtoAI)['message']}'
+        });
+      }
     }
 
     TDXtoAI = '${jsonDecode(AItoMAP)['data']}$TDXtoAI';
